2c558e3016c2a6065cc93d750a127ec8805b4b77
Taraansh <taraanshnandwani123@gmail.com>
2023-03-02 20:24:23 +0530

    Handled the response from form

--- home/views.py
+++ home/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from home.models import Task
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    context = {'success': False}
+    if request.method == "POST":
+        title = request.POST['title']
+        desc = request.POST['desc']
+        ins = Task(taskTitle=title, taskDescription=desc)
+        ins.save()
+        context = {'success': True}
+
+    return render(request, 'index.html', context)
 
 
 def tasks(request):
